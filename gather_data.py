9003c202f2b1cadbb85d54499f18d9be5aa0f2af
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-04-14 09:37:04 -0600

    modify get data

--- gather_data.py
+++ gather_data.py
@@ -10,10 +10,19 @@ def main():
     fnames = glob.glob("*.log")
     plt_pfx = "plt117501"
     lst = []
+
+    # Get the number of fields from the native plt file
+    with open(f"{plt_pfx}-native/Header", "r") as f:
+        f.readline()
+        nfields = int(f.readline())
+
     for fname in fnames:
         name = fname.replace(".log", "")
         with open(fname, "r") as f:
+            ncells = 0
             for line in f:
+                if "grids" in line and "cells" in line:
+                    ncells += int(line.split("cells")[0].split()[-1])
                 if "amr-wind::IOManager::write_plot_file" in line:
                     time = float(line.split()[3])
 
@@ -22,7 +31,15 @@ def main():
             sze = sum(f.stat().st_size for f in plt_path.glob("**/*") if f.is_file())
         elif plt_path.is_file():
             sze = plt_path.stat().st_size
-        lst.append({"name": name, "time": time, "size": sze})
+        lst.append(
+            {
+                "name": name,
+                "time": time,
+                "size": sze,
+                "ncells": ncells,
+                "nfields": nfields,
+            }
+        )
     keys = lst[0].keys()
     fname = "data.csv"
     with open(fname, "w", newline="") as f:
