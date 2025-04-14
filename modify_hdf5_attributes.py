a9b4f4e265afeee515c869786d11e301f85d1e68
Prakash Mohan <Prakash.Mohan@nrel.gov>
2025-04-14 11:48:17 -0600

    Modify one more attribute to make multilevel work

--- modify_hdf5_attributes.py
+++ modify_hdf5_attributes.py
@@ -9,7 +9,7 @@ import numpy as np
 def modify_attributes(fname):
     """Modifies HDF5 attributes from amrex plt files to be compatible with yt"""
     hf = h5py.File(fname, "r+")
-    attr_keys = ["dim", "num_levels"]
+    attr_keys = ["dim", "num_levels", "finest_level"]
     for key in attr_keys:
         if np.ndim(hf.attrs[key]):
             hf.attrs.__setitem__(key, hf.attrs[key].item())
