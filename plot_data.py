75949d5870d203488134a3afdb0177cac6cd889f
Marc Henry de Frahan <marc.henrydefrahan@nrel.gov>
2025-04-14 10:21:47 -0600

    more plots

--- plot_data.py
+++ plot_data.py
@@ -18,12 +18,18 @@ def bytes_to_mb(bytes):
 def main():
     fname = "data.csv"
     df = pd.read_csv(fname)
+    df["time_dof"] = df["time"] / (df["ncells"] * df["nfields"])
+    df["size_dof"] = df["size"] / (df["ncells"] * df["nfields"])
     df["size"] = df["size"].apply(bytes_to_mb)
+    native_size = df.loc[df["name"] == "native", "size"].values[0]
+    native_time = df.loc[df["name"] == "native", "time"].values[0]
+    df["time_norm"] = df["time"] / native_time
+    df["size_norm"] = df["size"] / native_size
     df.sort_values(by=["name"], inplace=True)
     print(df)
 
     pname = "plots.pdf"
-    cols = ["time", "size"]
+    cols = ["time", "size", "time_dof", "size_dof", "time_norm", "size_norm"]
     for col in cols:
         plt.figure(f"{col}", figsize=(14, 6))
         ax = plt.gca()
@@ -46,6 +52,22 @@ def main():
 
         plt.figure("size")
         plt.xlabel(r"Size $[MB]$", fontsize=22, fontweight="bold")
+        pdf.savefig(dpi=300)
+
+        plt.figure("time_dof")
+        plt.xlabel(r"Time $[s/DoF]$", fontsize=22, fontweight="bold")
+        pdf.savefig(dpi=300)
+
+        plt.figure("size_dof")
+        plt.xlabel(r"Size $[B/DoF]$", fontsize=22, fontweight="bold")
+        pdf.savefig(dpi=300)
+
+        plt.figure("time_norm")
+        plt.xlabel(r"Time $[-]$", fontsize=22, fontweight="bold")
+        pdf.savefig(dpi=300)
+
+        plt.figure("size_norm")
+        plt.xlabel(r"Size $[-]$", fontsize=22, fontweight="bold")
         pdf.savefig(dpi=300)
 
 
